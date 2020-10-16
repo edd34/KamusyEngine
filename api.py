31729aaa6406b4d8ce4badfb272ed60e61eacaee
Eddine Omar <eddine.farid@hotmail.fr>
2020-10-17 01:09:00 +0200

    create CRUD api for endpoint /word

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from flask import Flask, render_template, url_for, request, redirect
+from flask import jsonify
 from .shared_models import db
 from .app_models import Word
 
@@ -12,8 +13,6 @@ def index():
         new_word = Word(name=left_word)
 
         try:
-            # import pdb
-            # pdb.set_trace()
             db.session.add(new_word)
             db.session.commit()
             return redirect('/')
@@ -50,3 +49,38 @@ def update(id):
 
     else:
         return render_template('update.html', word=word)
+
+@api_component.route('/words/', methods=['GET'])
+def get_all_words():
+    words_query = Word.query.order_by(Word.name)
+    json_list=[i.serialize for i in words_query.all()]
+    return jsonify(json_list), 200
+
+@api_component.route('/word/<int:id>', methods=['GET'])
+def get_word(id):
+    word = Word.query.filter_by(id=id).one()
+    return jsonify(id=word.id, name=word.name, language=word.language, date=word.date_created), 200
+
+@api_component.route('/word/', methods=['PSOT'])
+def add_word(id):
+    body = request.json()
+    word = Word(name=body.name, language=body.language)
+    db.session.add(word)
+    db.session.commit()
+    return jsonify(id=word.id, name=word.name, language=word.language, date=word.date_created), 200
+
+@api_component.route('/word/<int:id>', methods=['UPDATE'])
+def update_word(id):
+    word = Word.query.filter_by(id=id).one()
+    body = request.json()
+    if body.name is not None:
+        word.name = body.name
+    if body.language is not None:
+        word.language = body.language
+    db.session.commit()
+    return jsonify(id=word.id, name=word.name, language=word.language, date=word.date_created), 200
+
+@api_component.route('/word/<int:id>', methods=['DELETE'])
+def delete_word(id):
+    Word.query.filter_by(id=id).delete()
+    return jsonify(), 200
