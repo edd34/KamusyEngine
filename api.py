36099e0edf0e884a71a7b0f526925f52081a7ae2
Eddine Omar <eddine.farid@hotmail.fr>
2020-10-18 02:31:02 +0200

    setup CORS and remove server rendered page

--- api.py
+++ api.py
@@ -1,68 +1,72 @@
 from flask import Blueprint
 from flask import Flask, render_template, url_for, request, redirect
+from flask_cors import CORS, cross_origin
 from flask import jsonify
 from .shared_models import db
 from .app_models import Word, Languages, Dict
 
 api_component = Blueprint("api", __name__)
 
-@api_component.route('/', methods=['POST', 'GET'])
-def index():
-    if request.method == 'POST':
-        left_word = request.form['name']
-        new_word = Word(name=left_word)
+# @api_component.route('/', methods=['POST', 'GET'])
+# def index():
+#     if request.method == 'POST':
+#         left_word = request.form['name']
+#         new_word = Word(name=left_word)
 
-        try:
-            db.session.add(new_word)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task'
+#         try:
+#             db.session.add(new_word)
+#             db.session.commit()
+#             return redirect('/')
+#         except:
+#             return 'There was an issue adding your task'
 
-    else:
-        words = Word.query.order_by(Word.date_created).all()
-        return render_template('index.html', words=words)
+#     else:
+#         words = Word.query.order_by(Word.date_created).all()
+#         return render_template('index.html', words=words)
 
 
-@api_component.route('/delete/<int:id>')
-def delete(id):
-    word_to_delete = Word.query.get_or_404(id)
-    try:
-        db.session.delete(word_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was a problem deleting that word'
+# @api_component.route('/delete/<int:id>')
+# def delete(id):
+#     word_to_delete = Word.query.get_or_404(id)
+#     try:
+#         db.session.delete(word_to_delete)
+#         db.session.commit()
+#         return redirect('/')
+#     except:
+#         return 'There was a problem deleting that word'
 
-@api_component.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    word = Word.query.get_or_404(id)
+# @api_component.route('/update/<int:id>', methods=['GET', 'POST'])
+# def update(id):
+#     word = Word.query.get_or_404(id)
 
-    if request.method == 'POST':
-        word.name = request.form['name']
+#     if request.method == 'POST':
+#         word.name = request.form['name']
 
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
+#         try:
+#             db.session.commit()
+#             return redirect('/')
+#         except:
+#             return 'There was an issue updating your task'
 
-    else:
-        return render_template('update.html', word=word)
+#     else:
+#         return render_template('update.html', word=word)
 
 # Words API
 @api_component.route('/words/', methods=['GET'])
+@cross_origin()
 def get_all_words():
     words_query = Word.query.order_by(Word.name)
     json_list=[i.serialize for i in words_query.all()]
     return jsonify(json_list), 200
 
 @api_component.route('/word/<int:id>', methods=['GET'])
+@cross_origin()
 def get_word(id):
     word = Word.query.filter_by(id=id).one()
     return jsonify(id=word.id, name=word.name, language=word.language, date=word.date_created), 200
 
 @api_component.route('/word/', methods=['POST'])
+@cross_origin()
 def add_word():
     body = request.get_json()
     word = Word(name=body["name"], language=body["language"])
@@ -71,6 +75,7 @@ def add_word():
     return jsonify(id=word.id, name=word.name, language=word.language, date=word.date_created), 200
 
 @api_component.route('/word/<int:id>', methods=['PATCH'])
+@cross_origin()
 def update_word(id):
     word = Word.query.filter_by(id=id).one()
     body = request.get_json()
@@ -82,18 +87,21 @@ def update_word(id):
     return jsonify(id=word.id, name=word.name, language=word.language, date=word.date_created), 200
 
 @api_component.route('/word/<int:id>', methods=['DELETE'])
+@cross_origin()
 def delete_word(id):
     Word.query.filter_by(id=id).delete()
     return jsonify(), 200
 
 # Language API
 @api_component.route('/languages/', methods=['GET'])
+@cross_origin()
 def get_all_languages():
     languages_query = Languages.query.order_by(Languages.name)
     json_list=[i.serialize for i in languages_query.all()]
     return jsonify(json_list), 200
 
 @api_component.route('/language/<int:id>', methods=['GET'])
+@cross_origin()
 def get_language(id):
     language = Languages.query.filter_by(id=id).one()
     return jsonify(id=language.id, name=language.name, date=language.date_created), 200
@@ -107,6 +115,7 @@ def add_language():
     return jsonify(id=language.id, name=language.name, date=language.date_created), 200
 
 @api_component.route('/language/<int:id>', methods=['PATCH'])
+@cross_origin()
 def update_language(id):
     language = Languages.query.filter_by(id=id).first()
     body = request.get_json()
@@ -116,12 +125,14 @@ def update_language(id):
     return jsonify(id=language.id, name=language.name, date=language.date_created), 200
 
 @api_component.route('/language/<int:id>', methods=['DELETE'])
+@cross_origin()
 def delete_language(id):
     Languages.query.filter_by(id=id).delete()
     return jsonify(), 200
 
 # dict api
 @api_component.route('/dict', methods=['POST'])
+@cross_origin()
 def add_translation():
     body = request.get_json()
 
@@ -144,6 +155,7 @@ def add_translation():
 
 # dict api
 @api_component.route('/dicts', methods=['GET'])
+@cross_origin()
 def get_all_translation():
     res = []
     dict_id = Dict.query.all()
--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, url_for, request, redirect
+from flask_cors import CORS
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from .api import api_component
 from .shared_models import db, ma
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kamusy.db'
+app.config['CORS_HEADERS'] = 'Access-Control-Allow-Origin'
 app.register_blueprint(api_component)
 db.init_app(app)
 
@@ -13,5 +16,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    
     app.run(debug=True)
