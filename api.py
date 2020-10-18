de8a35fd301a6284a29b28b90d7800a6330e0844
Eddine Omar <eddine.farid@hotmail.fr>
2020-10-18 08:44:58 +0200

    add function get_or_create and get_or_add fix get all languages (compute also nb words) remove comments

--- api.py
+++ api.py
@@ -2,54 +2,11 @@ from flask import Blueprint
 from flask import Flask, render_template, url_for, request, redirect
 from flask_cors import CORS, cross_origin
 from flask import jsonify
-from .shared_models import db
+import json
+from .shared_models import db, get_or_add
 from .app_models import Word, Languages, Dict
 
 api_component = Blueprint("api", __name__)
-
-# @api_component.route('/', methods=['POST', 'GET'])
-# def index():
-#     if request.method == 'POST':
-#         left_word = request.form['name']
-#         new_word = Word(name=left_word)
-
-#         try:
-#             db.session.add(new_word)
-#             db.session.commit()
-#             return redirect('/')
-#         except:
-#             return 'There was an issue adding your task'
-
-#     else:
-#         words = Word.query.order_by(Word.date_created).all()
-#         return render_template('index.html', words=words)
-
-
-# @api_component.route('/delete/<int:id>')
-# def delete(id):
-#     word_to_delete = Word.query.get_or_404(id)
-#     try:
-#         db.session.delete(word_to_delete)
-#         db.session.commit()
-#         return redirect('/')
-#     except:
-#         return 'There was a problem deleting that word'
-
-# @api_component.route('/update/<int:id>', methods=['GET', 'POST'])
-# def update(id):
-#     word = Word.query.get_or_404(id)
-
-#     if request.method == 'POST':
-#         word.name = request.form['name']
-
-#         try:
-#             db.session.commit()
-#             return redirect('/')
-#         except:
-#             return 'There was an issue updating your task'
-
-#     else:
-#         return render_template('update.html', word=word)
 
 # Words API
 @api_component.route('/words/', methods=['GET'])
@@ -96,9 +53,17 @@ def delete_word(id):
 @api_component.route('/languages/', methods=['GET'])
 @cross_origin()
 def get_all_languages():
-    languages_query = Languages.query.order_by(Languages.name)
-    json_list=[i.serialize for i in languages_query.all()]
-    return jsonify(json_list), 200
+    all_languages = Languages.query.order_by(Languages.name).all()
+    res = []
+    for l in all_languages:
+        query = db.session.query(Languages, db.func.count(Word.id)).join(Languages, Word.language == Languages.id).filter_by(id =l.id).group_by(Word.language).first()
+        if query == None:
+            query = 0
+        else:
+            query = query[1]
+        res.append(dict(language=l.serialize, count=query))
+    return jsonify(res), 200
+
 
 @api_component.route('/language/<int:id>', methods=['GET'])
 @cross_origin()
@@ -131,22 +96,18 @@ def delete_language(id):
     return jsonify(), 200
 
 # dict api
-@api_component.route('/dict', methods=['POST'])
+@api_component.route('/dicts', methods=['POST'])
 @cross_origin()
 def add_translation():
     body = request.get_json()
 
-    language1 = Languages(name=body["language1"])
-    language2 = Languages(name=body["language2"])
-    db.session.add(language1)
-    db.session.add(language2)
-    db.session.commit()
-    
-    word1 = Word(name=body["word1"], language=language1.id)
-    word2 = Word(name=body["word2"], language=language2.id)
-    db.session.add(word1)
-    db.session.add(word2)
-    db.session.commit()
+    language1 = Languages.query.filter_by(id=body["id_language1"]).first()
+    language2 = Languages.query.filter_by(id=body["id_language2"]).first()
+
+    word1 = get_or_add(Word,name=body["word1"], language=language1.id)
+    word2 = get_or_add(Word,name=body["word2"], language=language2.id)
+
+    db.session.flush()
 
     translation = Dict(id_word1=word1.id, id_word2=word2.id)
     db.session.add(translation)
@@ -163,9 +124,9 @@ def get_all_translation():
     for id in dict_id:
         tmp = {}
         current_word = Word.query.filter_by(id=id.id_word1).first()
-        current_word_language = Languages.query.filter_by(id=current_word.id).first()
+        current_word_language = Languages.query.filter_by(id=current_word.language).first()
         translated_word = Word.query.filter_by(id=id.id_word2).first()
-        translated_word_language = Languages.query.filter_by(id=translated_word.id).first()
+        translated_word_language = Languages.query.filter_by(id=translated_word.language).first()
         tmp["current_word_name"] = current_word.name
         tmp["current_word_language"] = current_word_language.name
         tmp["translated_word_name"] = translated_word.name
--- shared_models.py
+++ shared_models.py
@@ -2,3 +2,28 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 db = SQLAlchemy()
 ma = Marshmallow()
+
+def get_or_add(model, **kwargs):
+    instance = db.session.query(model).filter_by(**kwargs).first()
+    if instance:
+        return instance
+    else:
+        instance = model(**kwargs)
+        db.session.add(instance)
+        return instance
+
+def get_or_create(model, **kwargs):
+    instance = db.session.query(model).filter_by(**kwargs).first()
+    if instance:
+        return instance
+    else:
+        instance = model(**kwargs)
+        db.session.add(instance)
+        db.session.commit()
+        return instance
+
+def dump_datetime(value):
+    """Deserialize datetime object into string form for JSON processing."""
+    if value is None:
+        return None
+    return [value.strftime("%Y-%m-%d"), value.strftime("%H:%M:%S")]
