105fb9c16f8005f27fc9ee96d58e224ce18968d8
Eddine Omar <eddine.farid@hotmail.fr>
2020-10-20 01:23:07 +0200

    move initialization methods into app folder

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,26 @@ from flask_marshmallow import Marshmallow
 
 db = SQLAlchemy()
 ma = Marshmallow()
+
+from flask import Flask
+from flask_cors import CORS
+from app.api.dict_api import dict_api_component
+from app.api.language_api import language_api_component
+from app.api.word_api import word_api_component
+# from app import db
+
+def create_app(db_uri):
+    app = Flask(__name__)
+    app.config['CORS_HEADERS'] = 'Access-Control-Allow-Origin'
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    CORS(app)
+    db.init_app(app)
+    register_blueprint(app)
+    with app.app_context():
+        db.create_all()
+    return app
+
+def register_blueprint(app):
+    bp_components = [dict_api_component, language_api_component, word_api_component]
+    for component in bp_components:
+        app.register_blueprint(component)
--- main.py
+++ main.py
@@ -1,25 +1,10 @@
 """
     This is the main entry for KamusyEngine
 """
+from app import create_app, register_blueprint
 
-from flask import Flask
-from flask_cors import CORS
-from app.api.dict_api import dict_api_component
-from app.api.language_api import language_api_component
-from app.api.word_api import word_api_component
-from app import db
-app = Flask(__name__)
-CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kamusy.db'
-app.config['CORS_HEADERS'] = 'Access-Control-Allow-Origin'
-db.init_app(app)
-
-bp_components = [dict_api_component, language_api_component, word_api_component]
-for component in bp_components:
-    app.register_blueprint(component)
-
-with app.app_context():
-    db.create_all()
+DATABASE_URI = 'sqlite:///kamusy.db'
 
 if __name__ == "__main__":
+    app = create_app(DATABASE_URI)
     app.run(debug=True)
