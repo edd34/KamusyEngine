180a62a481455deb10c0df604b33300f752493b4
Eddine Omar <eddine.farid@hotmail.fr>
2020-10-30 15:46:23 +0100

    update word model

--- app/models/word_model.py
+++ app/models/word_model.py
@@ -2,12 +2,14 @@ from datetime import datetime
 from marshmallow import fields
 from app import db, ma
 from app.models import dump_datetime
+from app.models.language_model import Languages
 
 class Word(db.Model):
     """Represent a word in the dictionary"""
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
-    language = db.Column(db.Integer)
+    language_id = db.Column(db.Integer, db.ForeignKey('languages.id'), nullable=False)
+    language = db.relationship('Languages', uselist=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     def __repr__(self):
         return '<Word %r>' % self.id
@@ -18,11 +20,12 @@ class Word(db.Model):
         return {
            'id' : self.id,
            'date_created': dump_datetime(self.date_created),
-           'language': self.language,
+           'language_id': self.language_id,
+           'language'
            'name': self.name
         }
 
 class WordShema(ma.Schema):
     """Word schema for validation with Marshmallow"""
     name = fields.String()
-    language = fields.String()
+    language_id = fields.Integer()
