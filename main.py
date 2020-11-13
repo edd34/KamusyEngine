4ce8f2dc593c9ef02fd1b4b42cbcba4b740dabfa
Eddine Omar <eddine.farid@hotmail.fr>
2020-11-13 17:48:28 +0100

    use config parser

--- main.py
+++ main.py
@@ -2,11 +2,16 @@
     This is the main entry for KamusyEngine
 """
 from app import create_app, enable_CORS, register_blueprint, init_db
-
+from configparser import ConfigParser
 DATABASE_URI = 'sqlite:///kamusy.db'
+
+import json
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
 if __name__ == "__main__":
     app = create_app(DATABASE_URI)
+    app.config.update(config["DEFAULT"])
     init_db(app)
     enable_CORS(app)
     register_blueprint(app)
